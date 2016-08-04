688d577578b4a5889d8cbf3cb14c2b3f199e1823
Helios <helios157@gmail.com>
2016-08-04 08:08:11 +0200

    Expanded gui

--- hexapod/src/test/java/ch/sharpsoft/hexapod/ServoManagerTest.java
+++ hexapod/src/test/java/ch/sharpsoft/hexapod/ServoManagerTest.java
@@ -28,7 +28,7 @@ public class ServoManagerTest {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setBounds(0, 0, 1024, 1024);
 		final JPanel mainframe = new JPanel();
-		mainframe.setLayout(new GridLayout(9, 1));
+		mainframe.setLayout(new GridLayout(12, 1));
 		for (final Leg l : hp.getLegs()) {
 			final JPanel line = new JPanel();
 			final JButton angles = new JButton();
@@ -106,6 +106,257 @@ public class ServoManagerTest {
 
 			mainframe.add(line);
 		}
+
+		{
+			final JPanel line = new JPanel();
+			final JButton angles = new JButton();
+			angles.setText("Odd ");
+			angles.setFont(f);
+			line.add(angles);
+
+			final JTextField kee0 = new JTextField(15);
+			kee0.setFont(f);
+			line.add(kee0);
+
+			final JTextField kee1 = new JTextField(15);
+			kee1.setFont(f);
+			line.add(kee1);
+
+			final JTextField kee2 = new JTextField(15);
+			kee2.setFont(f);
+			line.add(kee2);
+
+			final JButton endpoint = new JButton();
+			endpoint.setText("Move ");
+			endpoint.setFont(f);
+			line.add(endpoint);
+
+			final JTextField moveX = new JTextField(15);
+			moveX.setFont(f);
+			line.add(moveX);
+
+			final JTextField moveY = new JTextField(15);
+			moveY.setFont(f);
+			line.add(moveY);
+
+			final JTextField moveZ = new JTextField(15);
+			moveZ.setFont(f);
+			line.add(moveZ);
+
+			Runnable r = new Runnable() {
+
+				@Override
+				public void run() {
+					kee0.setText("0");
+					kee1.setText("0");
+					kee2.setText("0");
+					moveX.setText("0");
+					moveY.setText("0");
+					moveZ.setText("0");
+				}
+			};
+			r.run();
+			// reloads.add(r);
+
+			angles.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					Double a0 = Double.valueOf(kee0.getText());
+					Double a1 = Double.valueOf(kee1.getText());
+					Double a2 = Double.valueOf(kee2.getText());
+					hp.getLegs().stream().filter(l -> l.getId() % 2 == 1).forEach(l -> l.setAngles(a0, a1, a2));
+					reload();
+				}
+			});
+
+			endpoint.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					Double x = Double.valueOf(moveX.getText());
+					Double y = Double.valueOf(moveY.getText());
+					Double z = Double.valueOf(moveZ.getText());
+					hp.getLegs().stream().filter(l -> l.getId() % 2 == 1).forEach(l -> {
+						Vector3 old = l.getEndpoint();
+						old.add(new Vector3(x, y, z));
+						l.setEndpoint(old);
+					});
+					reload();
+				}
+			});
+
+			mainframe.add(line);
+		}
+
+		{
+			final JPanel line = new JPanel();
+			final JButton angles = new JButton();
+			angles.setText("Even ");
+			angles.setFont(f);
+			line.add(angles);
+
+			final JTextField kee0 = new JTextField(15);
+			kee0.setFont(f);
+			line.add(kee0);
+
+			final JTextField kee1 = new JTextField(15);
+			kee1.setFont(f);
+			line.add(kee1);
+
+			final JTextField kee2 = new JTextField(15);
+			kee2.setFont(f);
+			line.add(kee2);
+
+			final JButton endpoint = new JButton();
+			endpoint.setText("Move ");
+			endpoint.setFont(f);
+			line.add(endpoint);
+
+			final JTextField moveX = new JTextField(15);
+			moveX.setFont(f);
+			line.add(moveX);
+
+			final JTextField moveY = new JTextField(15);
+			moveY.setFont(f);
+			line.add(moveY);
+
+			final JTextField moveZ = new JTextField(15);
+			moveZ.setFont(f);
+			line.add(moveZ);
+
+			Runnable r = new Runnable() {
+
+				@Override
+				public void run() {
+					kee0.setText("0");
+					kee1.setText("0");
+					kee2.setText("0");
+					moveX.setText("0");
+					moveY.setText("0");
+					moveZ.setText("0");
+				}
+			};
+			r.run();
+			// reloads.add(r);
+
+			angles.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					Double a0 = Double.valueOf(kee0.getText());
+					Double a1 = Double.valueOf(kee1.getText());
+					Double a2 = Double.valueOf(kee2.getText());
+					hp.getLegs().stream().filter(l -> l.getId() % 2 == 0).forEach(l -> l.setAngles(a0, a1, a2));
+					reload();
+				}
+			});
+
+			endpoint.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					Double x = Double.valueOf(moveX.getText());
+					Double y = Double.valueOf(moveY.getText());
+					Double z = Double.valueOf(moveZ.getText());
+					hp.getLegs().stream().filter(l -> l.getId() % 2 == 0).forEach(l -> {
+						Vector3 old = l.getEndpoint();
+						old.add(new Vector3(x, y, z));
+						l.setEndpoint(old);
+					});
+					reload();
+				}
+			});
+
+			mainframe.add(line);
+		}
+
+		{
+			final JPanel line = new JPanel();
+			final JButton angles = new JButton();
+			angles.setText("All ");
+			angles.setFont(f);
+			line.add(angles);
+
+			final JTextField kee0 = new JTextField(15);
+			kee0.setFont(f);
+			line.add(kee0);
+
+			final JTextField kee1 = new JTextField(15);
+			kee1.setFont(f);
+			line.add(kee1);
+
+			final JTextField kee2 = new JTextField(15);
+			kee2.setFont(f);
+			line.add(kee2);
+
+			final JButton endpoint = new JButton();
+			endpoint.setText("Move ");
+			endpoint.setFont(f);
+			line.add(endpoint);
+
+			final JTextField moveX = new JTextField(15);
+			moveX.setFont(f);
+			line.add(moveX);
+
+			final JTextField moveY = new JTextField(15);
+			moveY.setFont(f);
+			line.add(moveY);
+
+			final JTextField moveZ = new JTextField(15);
+			moveZ.setFont(f);
+			line.add(moveZ);
+
+			Runnable r = new Runnable() {
+
+				@Override
+				public void run() {
+					kee0.setText("0");
+					kee1.setText("0");
+					kee2.setText("0");
+					moveX.setText("0");
+					moveY.setText("0");
+					moveZ.setText("0");
+				}
+			};
+			r.run();
+			// reloads.add(r);
+
+			angles.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					Double a0 = Double.valueOf(kee0.getText());
+					Double a1 = Double.valueOf(kee1.getText());
+					Double a2 = Double.valueOf(kee2.getText());
+					hp.getLegs().forEach(l -> l.setAngles(a0, a1, a2));
+					reload();
+				}
+			});
+
+			endpoint.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					Double x = Double.valueOf(moveX.getText());
+					Double y = Double.valueOf(moveY.getText());
+					Double z = Double.valueOf(moveZ.getText());
+					hp.getLegs().forEach(l -> {
+						Vector3 old = l.getEndpoint();
+						Vector3 n = old.add(new Vector3(x, y, z));
+						try {
+							l.setEndpoint(n);
+						} catch (IllegalArgumentException ex) {
+
+						}
+					});
+					reload();
+				}
+			});
+
+			mainframe.add(line);
+		}
+
 		window.getContentPane().add(mainframe);
 		window.pack();
 		window.setVisible(true);
