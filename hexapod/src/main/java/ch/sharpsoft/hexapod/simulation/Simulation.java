4b994c1a28ea6c860fe13c53fadd923822c51185
helios57 <helios157@gmail.com>
2016-11-03 20:34:01 +0100

    Added Simulation

---
+++ hexapod/src/main/java/ch/sharpsoft/hexapod/simulation/Simulation.java
@@ -0,0 +1,54 @@
+package ch.sharpsoft.hexapod.simulation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ch.sharpsoft.hexapod.Hexapod;
+import ch.sharpsoft.hexapod.Vector3;
+import ch.sharpsoft.hexapod.WalkSafe;
+
+public class Simulation {
+	private Hexapod hp;
+	private WalkSafe ws;
+
+	public Simulation() {
+		hp = new Hexapod();
+		ws = new WalkSafe(hp);
+		ws.setDirection(new Vector3(1, 0, 0));
+	}
+
+	public void run() {
+		List<Vector3> lastEndpoints = getEndpoints();
+		Double ground = lastEndpoints.stream().map(e -> e.getZ()).collect(Collectors.averagingDouble(d -> d)) + 0.5f;
+		System.out.println("ground " + ground);
+		List<Vector3> groundVectors = new ArrayList<>(6);
+		for (int i = 0; i < 10; i++) {
+			groundVectors.clear();
+
+			ws.doNextAction();
+			List<Vector3> currentEndpoints = getEndpoints();
+			for (int j = 0; j < currentEndpoints.size(); j++) {
+				Vector3 last = lastEndpoints.get(j);
+				Vector3 current = currentEndpoints.get(j);
+				if (last.getZ() < ground && current.getZ() < ground) {
+					groundVectors.add(current.substract(last));
+				}
+			}
+			
+			for(Vector3 gv : groundVectors){
+				
+			}
+
+			lastEndpoints = currentEndpoints;
+		}
+	}
+
+	private List<Vector3> getEndpoints() {
+		return hp.getLegs().stream().map(l -> l.getEndpoint()).collect(Collectors.toList());
+	}
+
+	public static void main(String[] args) {
+		new Simulation().run();
+	}
+}
--- hexapod/src/test/java/ch/sharpsoft/hexapod/WalkSafeTest.java
+++ hexapod/src/test/java/ch/sharpsoft/hexapod/WalkSafeTest.java
@@ -1,7 +1,5 @@
 package ch.sharpsoft.hexapod;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class WalkSafeTest {
