9f94039af58ab263a991778173f78814eba595b4
Helios <helios157@gmail.com>
2017-08-16 22:01:23 +0200

    Walking in Simulation

--- ch.sharpsoft.hexapod.simulation/src/main/java/ch/sharpsoft/hexapod/simulation/demo/BulletTestHexapod.java
+++ ch.sharpsoft.hexapod.simulation/src/main/java/ch/sharpsoft/hexapod/simulation/demo/BulletTestHexapod.java
@@ -5,7 +5,9 @@ import java.awt.BorderLayout;
 import java.awt.HeadlessException;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
@@ -47,6 +49,7 @@ import com.badlogic.gdx.utils.Array;
 import ch.sharpsoft.hexapod.Hexapod;
 import ch.sharpsoft.hexapod.Leg;
 import ch.sharpsoft.hexapod.LegSegment;
+import ch.sharpsoft.hexapod.WalkSafe;
 
 /**
  * @see https://xoppa.github.io/blog/using-the-libgdx-3d-physics-bullet-wrapper-part2/
@@ -56,6 +59,7 @@ public class BulletTestHexapod extends JFrame implements ApplicationListener {
 	private static final long serialVersionUID = 1L;
 	private final static int sizeX = 1048;
 	private final static int sizeY = 748;
+	private final Set<Runnable> updateAngles = new HashSet<>();
 
 	public static void main(String[] args) throws Throwable {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -103,12 +107,15 @@ public class BulletTestHexapod extends JFrame implements ApplicationListener {
 	btDynamicsWorld dynamicsWorld;
 	btConstraintSolver constraintSolver;
 
-	Hexapod hp = new Hexapod();
+	private Hexapod hp = new Hexapod();
+	private WalkSafe walkSafe;
 
 	@Override
 	public void create() {
 		Bullet.init();
 
+		walkSafe = new WalkSafe(hp);
+		walkSafe.setDirection(new ch.sharpsoft.hexapod.util.Vector3(1, 0, 0));
 		modelBatch = new ModelBatch();
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
@@ -179,11 +186,11 @@ public class BulletTestHexapod extends JFrame implements ApplicationListener {
 			ch.sharpsoft.hexapod.util.Quaternion orientation = segment1.getOrientation();
 			ch.sharpsoft.hexapod.util.Vector3 correction = orientation.multiply(new ch.sharpsoft.hexapod.util.Vector3(sizeLeg1.x, 0, 0));
 			ch.sharpsoft.hexapod.util.Vector3 startPoint = segment1.getStartPoint().add(correction);
-			Quaternion orientation2 = new Quaternion();
-			orientation2.set((float) orientation.getX(), (float) orientation.getZ(), (float) orientation.getY(), (float) -orientation.getW());
+			Quaternion orientation1 = new Quaternion();
+			orientation1.set((float) orientation.getX(), (float) orientation.getZ(), (float) orientation.getY(), (float) -orientation.getW());
 			leg1.transform.translate(0f, 9f, 0f);
 			leg1.transform.translate(toVector3(startPoint));
-			leg1.transform.rotate(orientation2);
+			leg1.transform.rotate(orientation1);
 
 			leg1.body.proceedToTransform(leg1.transform);
 			leg1.body.setUserValue(instances.size);
@@ -195,18 +202,85 @@ public class BulletTestHexapod extends JFrame implements ApplicationListener {
 			leg1.body.setContactCallbackFilter(GROUND_FLAG);
 
 			Vector3 pivotInA = toVector3(startPoint);
-			Vector3 pivotInB = new Vector3(sizeLeg1.x, 0, 0);
+			Vector3 pivotInB = new Vector3(-sizeLeg1.x, 0, 0);
 			Vector3 axisInA = new Vector3(0f, -1f, 0f);
 			Vector3 axisInB = new Vector3(0f, -1f, 0f);
-			btHingeConstraint hinge = new btHingeConstraint(main.body, leg1.body, pivotInA, pivotInB, axisInA, axisInB);
-			float yaw = (float) leg.getStartYaw() + (float) Math.PI;
-			hinge.setLimit(yaw, yaw);
-			hinge.enableAngularMotor(true, 1f, 1f);
+			btHingeConstraint hinge1 = new btHingeConstraint(main.body, leg1.body, pivotInA, pivotInB, axisInA, axisInB);
+			float yaw = (float) leg.getStartYaw();
+			hinge1.setLimit(yaw, yaw);
+			hinge1.enableAngularMotor(true, 1f, 1f);
 
-			dynamicsWorld.addConstraint(hinge, true);
+			dynamicsWorld.addConstraint(hinge1, true);
 
 			LegSegment segment2 = leg.getSegment2();
-			LegSegment endSegment = leg.getEndSegment();
+			Vector3 sizeLeg2 = new Vector3(7f * 0.5f, 1f * 0.5f, 3f * 0.5f);
+			SimulationObject leg2 = new SimulationObject(model, "leg2", new btBoxShape(sizeLeg2), 5f * 0.1f);
+			ch.sharpsoft.hexapod.util.Quaternion orientation2 = segment2.getOrientation();
+			ch.sharpsoft.hexapod.util.Vector3 correction2 = orientation2.multiply(new ch.sharpsoft.hexapod.util.Vector3(sizeLeg2.x, 0, 0));
+			ch.sharpsoft.hexapod.util.Vector3 startPoint2 = segment2.getStartPoint().add(correction2);
+			Quaternion orientation22 = new Quaternion();
+			orientation22.set((float) orientation2.getX(), (float) orientation2.getZ(), (float) orientation2.getY(), (float) -orientation2.getW());
+			leg2.transform.translate(0f, 9f, 0f);
+			leg2.transform.translate(toVector3(startPoint2));
+			leg2.transform.rotate(orientation22);
+
+			leg2.body.proceedToTransform(leg2.transform);
+			leg2.body.setUserValue(instances.size);
+			leg2.body.setCollisionFlags(leg2.body.getCollisionFlags() | btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
+			instances.add(leg2);
+
+			dynamicsWorld.addRigidBody(leg2.body);
+			leg2.body.setContactCallbackFlag(OBJECT_FLAG);
+			leg2.body.setContactCallbackFilter(GROUND_FLAG);
+			//
+			Vector3 pivotInA2 = new Vector3(sizeLeg1.x, 0, 0);
+			Vector3 pivotInB2 = new Vector3(-sizeLeg2.x, 0, 0);
+			Vector3 axisInA2 = new Vector3(0f, 0f, 1f);
+			Vector3 axisInB2 = new Vector3(0f, 0f, 1f);
+			btHingeConstraint hinge2 = new btHingeConstraint(leg1.body, leg2.body, pivotInA2, pivotInB2, axisInA2, axisInB2);
+			hinge2.setLimit(1f, 1f);
+			hinge2.enableAngularMotor(true, 1f, 1f);
+			//
+			dynamicsWorld.addConstraint(hinge2, true);
+
+			LegSegment segment3 = leg.getEndSegment();
+			Vector3 sizeLeg3 = new Vector3(13f * 0.5f, 1f * 0.5f, 3f * 0.5f);
+			SimulationObject leg3 = new SimulationObject(model, "leg3", new btBoxShape(sizeLeg3), 5f * 0.2f);
+			ch.sharpsoft.hexapod.util.Quaternion orientation3 = segment3.getOrientation();
+			ch.sharpsoft.hexapod.util.Vector3 correction3 = orientation.multiply(new ch.sharpsoft.hexapod.util.Vector3(sizeLeg3.x, 0, 0));
+			ch.sharpsoft.hexapod.util.Vector3 startPoint3 = segment3.getStartPoint().add(correction3);
+			Quaternion orientation33 = new Quaternion();
+			orientation33.set((float) orientation3.getX(), (float) orientation3.getZ(), (float) orientation3.getY(), (float) -orientation3.getW());
+			leg3.transform.translate(0f, 9f, 0f);
+			leg3.transform.translate(toVector3(startPoint3));
+			leg3.transform.rotate(orientation33);
+
+			leg3.body.proceedToTransform(leg3.transform);
+			leg3.body.setUserValue(instances.size);
+			leg3.body.setCollisionFlags(leg3.body.getCollisionFlags() | btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
+			instances.add(leg3);
+
+			dynamicsWorld.addRigidBody(leg3.body);
+			leg3.body.setContactCallbackFlag(OBJECT_FLAG);
+			leg3.body.setContactCallbackFilter(GROUND_FLAG);
+			//
+			Vector3 pivotInA3 = new Vector3(sizeLeg2.x, 0, 0);
+			Vector3 pivotInB3 = new Vector3(-sizeLeg3.x, 0, 0);
+			Vector3 axisInA3 = new Vector3(0f, 0f, 1f);
+			Vector3 axisInB3 = new Vector3(0f, 0f, 1f);
+			btHingeConstraint hinge3 = new btHingeConstraint(leg2.body, leg3.body, pivotInA3, pivotInB3, axisInA3, axisInB3);
+			hinge3.setLimit(0.5f, 0.5f);
+			hinge3.enableAngularMotor(true, 1f, 1f);
+			dynamicsWorld.addConstraint(hinge3, true);
+			updateAngles.add(() -> {
+				double[] angles = leg.getAngles();
+				float a1 = (float) (yaw + angles[0]);
+				float a2 = (float) (angles[1]);
+				float a3 = (float) (angles[2]);
+				hinge1.setLimit(a1, a1);
+				hinge2.setLimit(a2, a2);
+				hinge3.setLimit(a3, a3);
+			});
 		});
 
 	}
@@ -231,7 +305,9 @@ public class BulletTestHexapod extends JFrame implements ApplicationListener {
 		dynamicsWorld.stepSimulation(delta, 5, 1f / 60f);
 
 		if ((spawnTimer -= delta) < 0) {
-			spawnTimer = 1.5f;
+			walkSafe.doNextAction();
+			updateAngles.forEach(Runnable::run);
+			spawnTimer = 0.2f;
 		}
 
 		camController.update();
